ba84a379e3a1be425c0b365a261048244f80cd02
Bugra <71045060+TheGnc@users.noreply.github.com>
2023-02-10 19:22:18 +0300

    dwelling controller is created

---
+++ src/main/java/com/askidaevimproject/Ask/da/evim/olsun/controller/DwellingController.java
@@ -0,0 +1,11 @@
+package com.askidaevimproject.Ask.da.evim.olsun.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dwelling")
+public class DwellingController {
+
+
+}
