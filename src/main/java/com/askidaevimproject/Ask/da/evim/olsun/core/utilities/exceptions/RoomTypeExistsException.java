b556228976fcbfeb52724f0f1de3e06b00a66bc1
Bugra <71045060+TheGnc@users.noreply.github.com>
2023-03-03 17:17:20 +0300

    equals and hascode super true

--- src/main/java/com/askidaevimproject/Ask/da/evim/olsun/core/utilities/exceptions/RoomTypeExistsException.java
+++ src/main/java/com/askidaevimproject/Ask/da/evim/olsun/core/utilities/exceptions/RoomTypeExistsException.java
@@ -2,8 +2,10 @@ package com.askidaevimproject.Ask.da.evim.olsun.core.utilities.exceptions;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+@EqualsAndHashCode(callSuper = true)
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
