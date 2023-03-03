6b9f5b6ae58c34207ef755ff7ba521c61360d871
Bugra <71045060+TheGnc@users.noreply.github.com>
2023-03-03 13:49:54 +0300

    neighborhood response and districtname is completed fully.

--- src/main/java/com/askidaevimproject/Ask/da/evim/olsun/service/responses/GetAllNeighBorHoodResponse.java
+++ src/main/java/com/askidaevimproject/Ask/da/evim/olsun/service/responses/GetAllNeighBorHoodResponse.java
@@ -1,5 +1,18 @@
 package com.askidaevimproject.Ask.da.evim.olsun.service.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetAllNeighBorHoodResponse {
+
+    private Long neighborhood_id;
+    private String neighborhoodName;
+    private String districtName;
+    private String zipCode;
+
 
 }
--- src/main/java/com/askidaevimproject/Ask/da/evim/olsun/service/responses/GetByDistrictName.java
+++ src/main/java/com/askidaevimproject/Ask/da/evim/olsun/service/responses/GetByDistrictName.java
@@ -1,5 +1,22 @@
 package com.askidaevimproject.Ask.da.evim.olsun.service.responses;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class GetByDistrictName {
+
+
+    /*
+    Example DATA :
+    CityName:Ankara
+    Ankara -> Mamak
+    * **/
+    private String cityName;
+    private String districtName;
+
 
 }
